29efe803715fe9f418b24ee42d36c7e689b911a1
luuhai48 <39207570+luuhai48@users.noreply.github.com>
2018-07-19 11:04:42 +0000

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,6 @@
 <center><h1>Luu Hai - File Sharing</h1><br><br></center>
 <h2><a href="https://drive.google.com/file/d/1Sg9snnj9m6l6nzzs6STBwhpupqpOyEam/view">.minecraft</a></h2><br />
 <h2><a href="https://drive.google.com/open?id=13NPbdT7_N1H2fERGMZ9vtNGG_vrMa23x">.minecraft Shiginima</a></h2><br />
-<h2><a href="https://topg.org/Minecraft/in-465321">Aemine.vn Vote 1</a></h2><br />
-<h2><a href="https://minecraft-mp.com/server/172096/vote/">Aemine.vn Vote 2</a></h2><br />
-<h2><a href="https://drive.google.com/open?id=1KiXYRd1QNETrS4t8-kDBCpped3OgD-9K">Webserver</a></h2><br />
 <h2><a href="https://drive.google.com/drive/folders/1zsO0a-XjygjmSMSd4nay_dEZjrKYCLQ3?usp=sharing">Folder</a></h2><br />
 </body>
 </html>
