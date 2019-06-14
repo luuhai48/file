b4e42efba584c5d43bf034d25556026c980d9b30
luuhai48 <39207570+luuhai48@users.noreply.github.com>
2019-06-14 15:17:10 +0700

    Create js1.js

---
+++ js1.js
@@ -0,0 +1,4 @@
+function _get(e){for(var o=window.location.search.substr(1).split("&"),t={},n=0;n<o.length;n++){let e=o[n].split("=");t[decodeURIComponent(e[0])]=decodeURIComponent(e[1])}return t[e]}
+function setCookie(e,t,n){var i=new Date;i.setTime(i.getTime()+24*n*60*60*1e3);var o="expires="+i.toUTCString();document.cookie=e+"="+t+";"+o+";path=/"}
+function getCookie(e){for(var t=e+"=",n=document.cookie.split(";"),i=0;i<n.length;i++){for(var o=n[i];" "==o.charAt(0);)o=o.substring(1);if(0==o.indexOf(t))return o.substring(t.length,o.length)}return""}
+function _(el){return document.querySelector(el)}
