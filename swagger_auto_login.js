85ed7440ab498ea9075e9235f3f20ef321860e19
luuhai48 <39207570+luuhai48@users.noreply.github.com>
2021-08-06 13:27:26 +0700

    Version 3.0 - support openapi 3

--- swagger_auto_login.js
+++ swagger_auto_login.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Swagger Auto Login
 // @namespace    https://canteccouriers.com
-// @version      2.5
+// @version      3.0
 // @description  Remember Swagger Bearer token, auto refresh and signin
 // @author       hai.luu
 // @match        http://localhost/*
@@ -44,47 +44,50 @@ function findKey(obj, key) {
     "use strict";
 
     const init = () => {
+        var swagger_ui = null,
+            keyType = null;
+
         if (window.ui === undefined) {
-            setTimeout(init, 1000);
-            return;
+            try {
+                swagger_ui = ui;
+                keyType = "jwtAuth";
+            } catch (err) {
+                setTimeout(init, 1000);
+                return;
+            }
+        } else {
+            swagger_ui = window.ui;
+            keyType = "Bearer";
         }
 
         console.log("%cSwagger-Auto-Login Is Running", "color:white;background:red;font-size:20px;");
 
         addStyle(
-            `.sal--btn{outline:0;border:1px solid #ccc;border-radius:3px;padding:5px;cursor:pointer}.sal--btn.danger{color:#fff;background:red}.sal--btn.success{color:#fff;background:#4caf50}.sal--box{position:fixed;top:20px;right:20px;z-index:99999}.sal--box-content{width:250px;position:absolute;top:calc(100% + 5px);right:0;background:#fff;color:#000;border-radius:3px;padding:10px 15px;border:1px solid #ccc;display:none}.sal--block{display:block!important}.sal--input{width:100%;box-sizing:border-box;padding:4px 6px;margin-bottom:5px;border:1px solid #ccc}.sal--btn-submit{display:block;outline:0;border:none;border-radius:3px;padding:5px;cursor:pointer;color:#fff;background:#000;margin-top:10px}.sal--autorefresh{padding-left:5px}.sal--autorefresh input[type=checkbox]{float:left}`
+            `.sal--btn{outline:0;border:1px solid #ccc;border-radius:3px;padding:5px;cursor:pointer}.sal--btn.danger{color:#fff;background:red}.sal--btn.success{color:#fff;background:#4caf50}.sal--box{position:fixed;top:20px;right:20px;z-index:99999}.sal--box-content{width:250px;position:absolute;top:calc(100% + 5px);right:0;background:#fff;color:#000;border-radius:3px;padding:10px 15px;border:1px solid #ccc;display:none}.sal--block{display:block!important}.sal--input{width:100%;box-sizing:border-box;padding:4px 6px;margin-bottom:5px;border:1px solid #ccc}.sal--btn-submit{display:block;outline:0;border:none;border-radius:3px;padding:5px;cursor:pointer;color:#fff;background:#000;margin-top:10px}.sal--autorefresh{padding-left:5px}.sal--autorefresh input[type=checkbox]{float:left}.sal--select{width:100%;padding:5px;box-sizing:border-box;margin-bottom:10px;}`
         );
 
-        const key_prefix = window.location.pathname.includes("/admin") ? "admin_" : "",
-            keys = ["login_url", "refresh_url", "email", "password", "autorefresh"],
-            originalAuthorize = window.ui.authActions.authorize,
-            originalLogout = window.ui.authActions.logout,
+        var key_prefix = localGet("profile") == "admin" ? "admin_" : "";
+
+        const keys = ["login_url", "refresh_url", "email", "password", "autorefresh"],
+            originalAuthorize = swagger_ui.authActions.authorize,
+            originalLogout = swagger_ui.authActions.logout,
             box = document.createElement("div");
 
         var data = {};
-        for (let key of keys) {
-            data[key] = localGet(`${key_prefix}${key}`);
-        }
 
         box.classList.add("sal--box");
         box.innerHTML = `<button class="sal--btn" id="toggle_btn">SAL</button>
             <div class="sal--box-content">
+                <select class="sal--select" id="profile_select">
+                    <option value="default">Default profile</option>
+                    <option value="admin">Admin profile</option>
+                </select><br>
                 <form>
-                    <input class="sal--input" placeholder="Login URL" title="Login URL" value="${
-                        data.login_url || ""
-                    }" id="login_url_input" required="true">
-                    <input class="sal--input" placeholder="Refresh URL" title="Refresh URL" value="${
-                        data.refresh_url || ""
-                    }" id="refresh_url_input" required="true">
-                    <input class="sal--input" type="email" placeholder="Email" title="Email" value="${
-                        data.email || ""
-                    }" id="email_input" required="true">
-                    <input class="sal--input" type="text" placeholder="Password" title="Password" value="${
-                        data.password || ""
-                    }" id="password_input" required="true">
-                    <label><input type="checkbox" checked="${
-                        data.autorefresh || "false"
-                    }" id="autorefresh_input">Auto Refresh</label>
+                    <input class="sal--input" placeholder="Login URL" title="Login URL" id="login_url_input" required="true">
+                    <input class="sal--input" placeholder="Refresh URL" title="Refresh URL" id="refresh_url_input" required="true">
+                    <input class="sal--input" type="email" placeholder="Email" title="Email" id="email_input" required="true">
+                    <input class="sal--input" type="text" placeholder="Password" title="Password" id="password_input" required="true">
+                    <label><input type="checkbox" id="autorefresh_input">Auto Refresh</label>
                     <button class="sal--btn-submit" type="submit">Save & Refresh</button>
                 </form>
                 <hr>
@@ -97,6 +100,20 @@ function findKey(obj, key) {
                     <button class="sal--btn" id="refresh_token_btn" title="Copy" style="margin-bottom:5px; margin-left:5px;">Copy</button>
                 </div>
             </div>`;
+
+        const refresh_keys = () => {
+            data = {};
+            for (let key of keys) {
+                data[key] = localGet(`${key_prefix}${key}`);
+            }
+
+            _$("#login_url_input", box).value = data.login_url || "";
+            _$("#refresh_url_input", box).value = data.refresh_url || "";
+            _$("#email_input", box).value = data.email || "";
+            _$("#password_input", box).value = data.password || "";
+            _$("#autorefresh_input", box).checked = data.autorefresh || "false";
+        }
+        refresh_keys();
 
         _$("#toggle_btn", box).onclick = () => {
             _$(".sal--box-content", box).classList.toggle("sal--block");
@@ -117,7 +134,7 @@ function findKey(obj, key) {
             _$("#access_token", box).title = "Access token";
             _$("#refresh_token", box).title = "Refresh token";
         }
-        function error(message=null) {
+        function error(message = null) {
             resetMessage();
             _$("#toggle_btn", box).classList.add("danger");
             if (message) {
@@ -152,15 +169,27 @@ function findKey(obj, key) {
             }
         }
 
-        window.ui.authActions.authorize = function (payload) {
-            let token = payload.Bearer.value.replace("Bearer", "").replace("bearer", "").trim();
-            payload.Bearer.value = `Bearer ${token}`;
-            localSet(`${key_prefix}token`, token);
-            success();
+        swagger_ui.authActions.authorize = (payload) => {
+            let token = null;
+            if (payload.Bearer !== undefined) {
+                keyType = "Bearer";
+                token = payload.Bearer.value.replace("Bearer", "").replace("bearer", "").trim();
+                payload.Bearer.value = `Bearer ${token}`;
+            } else if (payload.jwtAuth !== undefined) {
+                keyType = "jwtAuth";
+                token = payload.jwtAuth.value.replace("Bearer", "").replace("bearer", "").trim();
+                payload.jwtAuth.value = `Bearer ${token}`;
+            }
+
+            if (token !== null) {
+                localSet(`${key_prefix}token`, token);
+                success();
+            }
+
             return originalAuthorize(payload);
         };
 
-        window.ui.authActions.logout = function (payload) {
+        swagger_ui.authActions.logout = (payload) => {
             localRem(`${key_prefix}token`);
             localRem(`${key_prefix}refresh`);
             _$("#access_token", box).value = "";
@@ -169,7 +198,7 @@ function findKey(obj, key) {
             return originalLogout(payload);
         };
 
-        function authorize(token) {
+        const authorize = (token) => {
             clearInterval(window.sal_check_lively);
             token = token.replace("bearer", "").replace("Bearer", "").trim();
 
@@ -181,8 +210,8 @@ function findKey(obj, key) {
                 console.error(err);
                 return login();
             }
-
-            let result = window.ui.preauthorizeApiKey("Bearer", `Bearer ${token}`);
+            token = keyType == "Bearer" ? `Bearer ${token}` : token;
+            let result = swagger_ui.preauthorizeApiKey(keyType || "Bearer", token);
             if (result === null) {
                 setTimeout(() => {
                     authorize(token);
@@ -207,19 +236,19 @@ function findKey(obj, key) {
             axios.post(data.refresh_url, {
                 refresh: localGet(`${key_prefix}refresh`)
             })
-            .then((res) => {
-                let access = findKey(res.data, "access");
+                .then((res) => {
+                    let access = findKey(res.data, "access");
 
-                localSet(`${key_prefix}token`, access);
+                    localSet(`${key_prefix}token`, access);
 
-                authorize(access);
-                success();
-            })
-            .catch((err) => {
-                console.error(err);
-                error(err.response.data.message);
-                return login();
-            });
+                    authorize(access);
+                    success();
+                })
+                .catch((err) => {
+                    console.error(err);
+                    error(err.response.data.message);
+                    return login();
+                });
         }
 
         function login() {
@@ -266,7 +295,7 @@ function findKey(obj, key) {
                 localSet(`${key_prefix}${key}`, data[key]);
             }
 
-            originalLogout(["Bearer"]);
+            originalLogout([keyType || "Bearer"]);
             login();
 
             _$(".sal--box-content", box).classList.remove("sal--block");
@@ -274,9 +303,20 @@ function findKey(obj, key) {
 
         document.body.appendChild(box);
 
-        let token = localGet(`${key_prefix}token`);
-        if (token) {
-            authorize(token);
+        const authen = () => {
+            let token = localGet(`${key_prefix}token`);
+            if (token) {
+                authorize(token);
+            }
+        }
+
+        authen();
+
+        _$("#profile_select", box).onchange = (e) => {
+            localSet("profile", e.target.value);
+            key_prefix = e.target.value == "admin" ? "admin_" : "";
+            refresh_keys();
+            authen();
         }
     };
     init();
